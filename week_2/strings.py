8842d7efe690c6d4f243bec9c014e24dc5f6e3b4
RoyKings61 <kingsroy61@gmail.com>
2024-02-23 09:52:25 +0300

    work1.1

---
+++ week_2/strings.py
@@ -0,0 +1,49 @@
+# Strings in python
+# Date : 22/02/2024
+# Name : Roy Kings
+
+city = "nairobi"
+
+# Convert to uppercase
+
+print(city)
+print("\n") # prints a new line
+print(city.upper())
+
+name = "JOEL PETER"
+print(name.lower())# This converts strings to lowercase
+
+town = "       Naivasha       "
+
+print(town)
+print("\t") # new tab
+print(town.strip())
+
+f_name = "Joel"
+s_name = "Peter"
+
+full_name = f_name + s_name
+print(full_name)
+
+print(city[0]) # a
+print(city[1]) # i
+print(city[2])
+print(city[3])
+print(city[-2])
+print(city[-1])
+print(city[6]) # i
+
+
+# Replacing a character
+
+fruit = "orangeoooooo"
+
+print(fruit.replace('o','Y'))
+
+subject = "Physicsal :sciences"
+
+print(subject.split(":"))
+
+age = 30
+height = 1.6 
+print("I am {0} years old and {1} meters tall ". format(age,height))
---
+++ week_2/volume of cylinder.py
@@ -0,0 +1,11 @@
+# simple programe to find surface area of a clinder
+# date : 20/02/2024
+# Name : Roy Kings
+
+p = 3.142
+r = float(input("enter radius value"))
+h = float(input("enter height value"))
+
+x = (p * r**2 *h)
+
+print("surface area of a cylinder is :", x)
